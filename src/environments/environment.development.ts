33c98d03f718eb1cb402916ed9b9ce6b4ba2329c
DAVID RODRIGUEZ <dsebastianrodriguez@ucundinamarca.edu.co>
2024-05-14 09:52:35 -0500

    Added changes

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    HOST: 'http://10.0.20.32',
-    IP_PRINCIPAL: '10.0.20.32',
+    HOST: 'http://127.0.0.1:8000',
+    IP_PRINCIPAL: '127.0.0.1:8000',
     TOKEN_AUTH: 'n1MdMJnUKDRCNc5LxXngcmeSuCzSQ8TuUnrg4g1DznPAUvi3D29R9QJAfWxb7HC6',
     TOKEN: 'token',
     REINTENTOS: 1,
